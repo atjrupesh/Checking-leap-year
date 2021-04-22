ad5742229e382f82e332f9e28ebeab89980f785a
Atj Rupesh <62754636+atjrupesh@users.noreply.github.com>
2021-04-22 11:21:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-# # 🚨 Don't change the code below 👇
-# year = int(input("Which year do you want to check? "))
-# # 🚨 Don't change the code above 👆
-
-# #Write your code below this line 👇
-# if (year%4 == 0):
-#   print("Leap year")
-# elif (year%100 == 0):
-#   print("Not Leap")
-# elif year%400 == 0:
-#   print("Leap year")
-# else:
-#   print("We don't talk anymore ") 
-
-# 🚨 Don't change the code below 👇
 year = int(input("Which year do you want to check? "))
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+
 if (year%4 == 0):
   print("Leap year")
 elif (year%100 != 0):
